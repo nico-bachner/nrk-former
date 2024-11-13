6e8ae36cc7aa4502c2b81e210939fc1497c475d9
Nico Bachner <git@nbac.me>
2024-11-13 19:32:15 +1100

    fix: broken import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from "next";
-import "@app/globals.css";
+import type { Metadata } from 'next'
+
+import '@/app/globals.css'
 
 export const metadata: Metadata = {
-  title: "NRK Former",
-  description: "Optimal solver for NRK Former",
-};
+  title: 'NRK Former',
+  description: 'Optimal solver for NRK Former',
+}
 
 type LayoutProps = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
 const Layout: React.FC<LayoutProps> = ({ children }) => (
   <html lang="en">
     <body className="antialiased">{children}</body>
   </html>
-);
+)
 
-export default Layout;
+export default Layout
