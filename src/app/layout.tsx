b9f91058409d63a201bea18aa88679d2304bc32f
Nico Bachner <git@nbac.me>
2024-11-13 19:27:39 +1100

    feat: initial scaffold

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "@app/globals.css";
+
+export const metadata: Metadata = {
+  title: "NRK Former",
+  description: "Optimal solver for NRK Former",
+};
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => (
+  <html lang="en">
+    <body className="antialiased">{children}</body>
+  </html>
+);
+
+export default Layout;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => (
+  <main>
+    <h1>NRK Former</h1>
+  </main>
+);
+
+export default Page;
