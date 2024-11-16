3b130c3d2e32ef10b702846e9023fd5c7e666c6e
Nico Bachner <git@nbac.me>
2024-11-16 16:14:40 +1100

    refactor: move board to component

---
+++ src/components/Board.tsx
@@ -0,0 +1,39 @@
+import { Tile } from '@/components/Tile'
+import { SquareIcon } from '@/icons/Square'
+import { Coordinate, TileGroup } from '@/types'
+
+type BoardProps = {
+  board: string[][]
+  hint?: TileGroup
+  onTileClick: (coordinate: Coordinate) => void
+}
+
+export const Board: React.FC<BoardProps> = ({ board, hint, onTileClick }) => (
+  <div className="flex">
+    {board.map((column, i) => (
+      <div key={i} className="flex flex-1 flex-col-reverse">
+        {column.map((tile, j) => {
+          if (tile == '') {
+            return <div key={j} className="aspect-square" />
+          }
+
+          return (
+            <button
+              key={j}
+              onClick={() => {
+                onTileClick([i, j])
+              }}
+              style={{ gridRow: 9 - j, gridColumn: i + 1 }}
+              className="relative"
+            >
+              <Tile tile={tile} />
+              {hint && hint.tiles.some(([i2, j2]) => i2 == i && j2 == j) && (
+                <SquareIcon className="absolute inset-0 h-full w-full text-white" />
+              )}
+            </button>
+          )
+        })}
+      </div>
+    ))}
+  </div>
+)
--- src/game.tsx
+++ src/game.tsx
@@ -2,14 +2,14 @@
 
 import { useEffect, useState } from 'react'
 
-import { Tile } from '@/Tile'
 import { BOARDS } from '@/boards'
 import { HintIcon } from '@/icons/Hint'
 import { RestartIcon } from '@/icons/Restart'
-import { SquareIcon } from '@/icons/Square'
 import { getHint } from '@/lib/getHint'
 import { getTileGroups } from '@/lib/getTileGroups'
 import { TileGroup } from '@/types'
+
+import { Board } from './components/Board'
 
 const BOARD = BOARDS[0].tiles
 
@@ -92,61 +92,34 @@ export const Game: React.FC = () => {
         </div>
       </div>
 
-      <div className="flex">
-        {board.map((column, i) => (
-          <div key={i} className="flex flex-1 flex-col-reverse">
-            {column.map((tile, j) => {
-              if (tile == '') {
-                return (
-                  <div
-                    key={j}
-                    style={{ gridRow: 9 - j, gridColumn: i + 1 }}
-                    className="aspect-square"
-                  />
-                )
-              }
+      <Board
+        board={board}
+        hint={hint}
+        onTileClick={([i, j]) => {
+          // must be a deep copy, otherwise useState doesn't detect the change
+          const newBoard = structuredClone(board)
 
-              return (
-                <button
-                  key={j}
-                  onClick={async () => {
-                    // must be [...board] to create a shallow copy, otherwise useState doesn't detect the change
-                    const newBoard = [...board]
+          const tileGroups = getTileGroups(board)
 
-                    const tileGroups = getTileGroups(board)
+          const tileGroup = tileGroups.find(({ tiles }) =>
+            tiles.some(([i2, j2]) => i2 == i && j2 == j),
+          )
 
-                    const tileGroup = tileGroups.find(({ tiles }) =>
-                      tiles.some(([i2, j2]) => i2 == i && j2 == j),
-                    )
+          if (tileGroup) {
+            tileGroup.tiles.forEach(([i, j]) => {
+              newBoard[i][j] = ''
+            })
 
-                    if (tileGroup) {
-                      tileGroup.tiles.forEach(([i, j]) => {
-                        newBoard[i][j] = ''
-                      })
+            newBoard.map((column) =>
+              column.sort((a, b) => (a == '' ? 1 : b == '' ? -1 : 0)),
+            )
 
-                      newBoard.map((column) =>
-                        column.sort((a, b) => (a == '' ? 1 : b == '' ? -1 : 0)),
-                      )
-
-                      setBoard(newBoard)
-                      setStepCount(stepCount + 1)
-                      setHint(undefined)
-                    }
-                  }}
-                  style={{ gridRow: 9 - j, gridColumn: i + 1 }}
-                  className="relative"
-                >
-                  <Tile tile={tile} />
-                  {hint &&
-                    hint.tiles.some(([i2, j2]) => i2 == i && j2 == j) && (
-                      <SquareIcon className="absolute inset-0 h-full w-full text-white" />
-                    )}
-                </button>
-              )
-            })}
-          </div>
-        ))}
-      </div>
+            setBoard(newBoard)
+            setStepCount(stepCount + 1)
+            setHint(undefined)
+          }
+        }}
+      />
     </div>
   )
 }
