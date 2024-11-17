3dd30233722934c7bb844f457284865563fb50b9
Nico Bachner <git@nbac.me>
2024-11-17 23:37:16 +1100

    fix: memoise hint algorithm

--- src/lib/getHint.ts
+++ src/lib/getHint.ts
@@ -9,33 +9,45 @@ import { getTileGroups } from './getTileGroups'
  * @param board The current board
  * @returns The collection of tiles to pop
  */
-export const getHint = (board: Board, depth = 3): TileGroup => {
+export const getHint = (
+  board: Board,
+  cache: { [key: string]: TileGroup } = {},
+  depth = 3,
+): TileGroup => {
+  if (Object.keys(cache).includes(JSON.stringify(board))) {
+    return cache[JSON.stringify(board)]
+  }
+
   const tileGroups = getTileGroups(board)
 
   if (depth == 0 || tileGroups.length == 1) {
-    return tileGroups.sort((a, b) => b.score - a.score)[0]
+    const bestNextMove = tileGroups.sort((a, b) => b.score - a.score)[0]
+
+    cache[JSON.stringify(board)] = bestNextMove
+
+    return bestNextMove
   }
 
-  return tileGroups
+  const bestNextMove = tileGroups
     .map(({ id, tiles, score }) => {
       const i = tiles[0][0]
       const j = tiles[0][1]
 
       const tileGroup = tileGroups.find(({ tiles }) =>
         tiles.some(([i2, j2]) => i2 == i && j2 == j),
-      )
-
-      if (!tileGroup) {
-        throw new Error('Tile group not found')
-      }
+      )!
 
       const newBoard = getNewBoard(board, tileGroup)
 
       return {
         id,
         tiles,
-        score: score + getHint(newBoard, depth - 1).score / 2,
+        score: score + getHint(newBoard, cache, depth - 1).score / 2,
       }
     })
     .sort((a, b) => b.score - a.score)[0]
+
+  cache[JSON.stringify(board)] = bestNextMove
+
+  return bestNextMove
 }
