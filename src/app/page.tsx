b47e11bd40d04d358d3a294c4b6f03b0fb1fa671
Nico Bachner <git@nbac.me>
2024-11-18 15:19:15 +1100

    feat: navigation menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-import { Game } from '@/game'
+import Link from 'next/link'
 
-const Page = () => <Game />
+import { BOARDS } from '@/boards'
+
+const Page = () => (
+  <div className="mx-auto flex max-w-xs flex-col gap-2 px-4 py-8">
+    {BOARDS.map(({ date }) => (
+      <Link
+        key={date}
+        href={`/${date}`}
+        className="rounded-lg bg-violet-700 px-4 py-2 text-center text-violet-100"
+      >
+        {new Date(date).toDateString()}
+      </Link>
+    ))}
+  </div>
+)
 
 export default Page
